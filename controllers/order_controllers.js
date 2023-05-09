75467b7653a3526f2fc7efa674534d8b31639fc1
AMRRAGHEB118 <amrragheb118@gmail.com>
2023-05-09 16:11:13 +0300

    add create order function and fetch all orders function

---
+++ controllers/order_controllers.js
@@ -0,0 +1,48 @@
+const ApiError = require('../utils/api_errors')
+const asyncHandler = require('express-async-handler')
+const order_model = require('../models/order')
+const cart_model = require('../models/cart')
+const product_model = require('../models/products')
+
+exports.create_order = asyncHandler(async (req, res, next) => {
+    const cart = await cart_model
+        .findOne({ user: req.user._id })
+        .populate('products.product')
+
+    if (!cart) {
+        return next(new ApiError(404, 'Cart not found'))
+    }
+
+    if (!req.body.shipping_address) {
+        return next(new ApiError(400, 'Shipping address is required'))
+    }
+
+    const products = cart.products.map((product) => ({
+        product: product.product._id,
+        price: product.price,
+        quantity: product.quantity,
+    }))
+
+    const total_price = cart.total_price
+    const shipping_address = req.body.shipping_address
+
+    const order = await order_model.create({
+        user: req.user._id,
+        products,
+        total_price,
+        shipping_address,
+    })
+    res.status(201).json({ data: order })
+})
+
+exports.get_orders = asyncHandler(async (req, res, next) => {
+    const orders = await order_model
+        .find({ user: req.user._id })
+        .populate('products.product')
+    if (orders.length === 0) {
+        return next(new ApiError(404, 'Orders not found'))
+    }
+    res.status(200).json({
+        data: orders,
+    })
+})

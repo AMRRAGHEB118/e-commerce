ff3aa4e95f7a50984caa201225490ba9c6be5566
AMRRAGHEB118 <amrragheb118@gmail.com>
2023-05-09 15:32:31 +0300

    fix cart route

--- routes/cart_route.js
+++ routes/cart_route.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
-const { get_cart } = require('../controllers/cart_controllers')
+const { get_cart, add_product_to_cart, remove_product_from_cart } = require('../controllers/cart_controllers')
 
-router.route('./').get(get_cart)
+router.route('/').get(get_cart) 
+
+router.route('/:product_id').post(add_product_to_cart).delete(remove_product_from_cart)
 
 module.exports = router
